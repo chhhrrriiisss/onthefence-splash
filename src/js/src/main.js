adc2620fdcc31551c727a9e069cc22b4a0a708c3
chrisnic <chris@chrisnicholls.co.nz>
2014-06-24 22:46:32 +1200

    Skel grid, svg, type fixes, plaque edits

--- src/js/src/main.js
+++ src/js/src/main.js
@@ -4,13 +4,20 @@
 */
 
 $(document).ready(function() {
-
 	
 	$('.header-wrapper').waypoint('sticky', {
 		offset: 300,
 		direction: 'down right',
 		stuckClass: 'stuck',
 		wrapper: '<div class="sticky-wrapper" />'
+	});
+
+	$('body').flowtype({
+		minimum   : 500,
+		maximum   : 1200,
+		minFont   : 10,
+		maxFont   : 17,
+		fontRatio : 35
 	});
 
 });
